24f23a403cff032f8ecbb1a267256b83348dd95b
migcruzz <101120553+migcruzz@users.noreply.github.com>
2024-05-29 11:28:05 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/Programa.dir/InteracaoUtilizador/interacao_utilizador.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Programa.dir/InteracaoUtilizador/interacao_utilizador.c.o: \
-  /Users/ruicarvalho/Desktop/EI/1ºano/2ºsemestre/Prog1/TrabalhoFinal/GestaoImobiliaria/InteracaoUtilizador/interacao_utilizador.c \
-  /Users/ruicarvalho/Desktop/EI/1ºano/2ºsemestre/Prog1/TrabalhoFinal/GestaoImobiliaria/InteracaoUtilizador/interacao_utilizador.h
